d2f107102d3a64791c2467a7769f141c23a6ab56
YovchoGandjurov <yovchoo@gmail.com>
2019-06-04 00:48:50 +0300

    Initial commit

---
+++ 05.Python Web and APIs - Twitter Login/login.py
@@ -0,0 +1,32 @@
+import constants
+import oauth2
+import urllib.parse as urlparse
+
+
+consumer = oauth2.Consumer(constants.CONSUMER_KEY,
+                           constants.CONSUMER_SECRET)
+client = oauth2.Client(consumer)
+
+response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
+
+if response.status != 200:
+    print('An error occurred getting the request token from Twitter!')
+
+request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print('Go to the following site in your browser:')
+print('{}?oauth_token={}'.format(constants.AUTHORIZATION_URL,
+                                 request_token['oauth_token']))
+
+oauth_verifier = input("What is the PIN? ")
+
+token = oauth2.Token(request_token['oauth_token'],
+                     request_token['oauth_token_secret'])
+token.set_verifier(oauth_verifier)
+
+client = oauth2.Client(consumer, token)
+
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print(access_token)
