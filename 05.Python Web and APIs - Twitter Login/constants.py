f00bc1b045482db09e4001b96822bd4d3cab547d
YovchoGandjurov <yovchoo@gmail.com>
2019-06-04 01:05:19 +0300

    updated gitignore file

--- 05.Python Web and APIs - Twitter Login/constants.py
+++
@@ -1,6 +0,0 @@
-CONSUMER_KEY = 'Br2uxsQxbVbL0X0kPlE5PMpI2'
-CONSUMER_SECRET = 'XmMrwNSIewak9clvB1BbcObsDIpajpHELFh0SKvWQtwnIJDbMq'
-
-REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
-ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
-AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
