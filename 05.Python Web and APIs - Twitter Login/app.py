0beed79a08911486719b77c6bca6d6e3795df2ea
YovchoGandjurov <yovchoo@gmail.com>
2019-06-04 17:39:19 +0300

    Creating the user profile, before_request and logout

--- 05.Python Web and APIs - Twitter Login/app.py
+++ 05.Python Web and APIs - Twitter Login/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, session, redirect, request
+from flask import Flask, render_template, session, redirect, request, url_for, \
+                  g
 from twitter_utils import get_request_token, get_oath_verifier_url, \
                           get_access_token
 from user import User
@@ -10,6 +11,12 @@ app.secret_key = '1234'
 Database.initialise()
 
 
+@app.before_request
+def load_user():
+    if 'screen_name' in session:
+        g.user = User.load_from_db_by_screen_name(session['screen_name'])
+
+
 @app.route('/')
 def homepage():
     return render_template('home.html')
@@ -17,10 +24,18 @@ def homepage():
 
 @app.route('/login/twitter')
 def twitter_login():
+    if 'screen_name' in session:
+        return redirect(url_for(profile))
     request_token = get_request_token()
     session['request_token'] = request_token
 
     return redirect(get_oath_verifier_url(request_token))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('homepage'))
 
 
 @app.route('/auth/twitter')
@@ -35,6 +50,11 @@ def twitter_auth():
         user.save_to_db()
 
     session['screen_name'] = user.screen_name
-    return user.screen_name
+    return redirect(url_for('profile'))
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', user=g.user)
 
 app.run(port=4995, debug=True)
