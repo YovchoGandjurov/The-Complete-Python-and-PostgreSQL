ed17af154476891569ebb6154f715d52c6fa69c9
YovchoGandjurov <yovchoo@gmail.com>
2019-06-07 02:23:12 +0300

    Added queue demo

---
+++ 07.Data Structures and Algorithms/Linked Queue/linkedqueue.py
@@ -0,0 +1,22 @@
+from linkedlist import LinkedList
+
+
+class LinkedQueue:
+
+    def __init__(self):
+        self.__linked_list = LinkedList()
+
+    def push(self, node):
+        self.__linked_list.add_start_to_list(node)
+
+    def pop(self):
+        return self.__linked_list.remove_end_from_list()
+
+    def find(self, name):
+        return self.__linked_list.find(name)
+
+    def print_details(self):
+        self.__linked_list.print_list()
+
+    def __len__(self):
+        return self.__linked_list.size()
